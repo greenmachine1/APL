55e82f27bc927594f0834352240701f0a6269123
Cory Green <thingy_shouldnt_be@Corys-MacBook-Pro.local>
2012-08-28 19:37:07 -0700

    added my files to my APL folder once again

---
+++ testApp/testApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Cory Green on 8/28/12.
+//  Copyright (c) 2012 Cory Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
