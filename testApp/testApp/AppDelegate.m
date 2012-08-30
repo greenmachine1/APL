7237b9a15bed68e598c3fa42aa4ef94ca1dfc203
Cory <greenmachine@fullsail.edu>
2012-08-30 03:14:16 -0700

    Trying to find a reason to use a nested loop in my scenario

--- testApp/testApp/AppDelegate.m
+++ testApp/testApp/AppDelegate.m
@@ -34,7 +34,7 @@
     float amountOfMoneyWeHave = 2690.90f;
     BOOL myVote = YES;
     
-    // explaining my dog situation
+// explaining my dog situation
     
     NSLog(@"My wife and I have an assortment of animals.  We have %d dogs and %d cats.", numberOfDogs, numberOfCats);
     NSLog(@"Every morning whenI wake up, I have to feed my dog Trouble.");
@@ -42,8 +42,8 @@
     NSLog(@"So, I have to check his pills every morning to make sure he has enough.");
     NSLog(@"He takes 2 a day and according to the remaining supply, he has enough for %d days", numberOfDaysWorthOfPills);
     
-    // what to do if I run out of pills
-    // usage of if statements
+// what to do if I run out of pills
+// usage of if statements
     if (dogPills == 2){
         NSLog(@"I currently have enough pills for another day for my dog.");
     }
@@ -59,11 +59,11 @@
     NSLog(@"Later that evening, my wife and I go through our finances to see if we have enough money to go on a cruise");
     NSLog(@"The amount we have is $%.2f but my wife told me to just round off the cents! So we have $%d", amountOfMoneyWeHave, (int)amountOfMoneyWeHave);
     
-    // example of if statement with boolean logic
+// example of if statement with boolean logic
     NSLog(@"She asks to go to the Bahamas");
     NSLog(@"And I say : ");
     
-    // using boolean logic in my if statements
+// using boolean logic in my if statements
     if((myVote == YES)  && (amountOfMoneyWeHave > 3500.00f)){
         NSLog(@"We have enough money, so yes! Lets go!");
     }
@@ -72,7 +72,7 @@
         NSLog(@"Sorry hunny, we dont have enough to go, lets pick somewhere else(sad face in real life).");
     }
     
-    // second choice for a cruise
+// second choice for a cruise
     NSLog(@"Well, what about the Caribbean?");
     
     if((myVote == NO) || ((amountOfMoneyWeHave <= 3000.00f) && (amountOfMoneyWeHave >= 2500.00f))){
@@ -88,68 +88,31 @@
 // my loops section
     NSLog(@"Later still, we go to sleep.  Our cat Scooter brings up toys from the downstairs.");
     NSLog(@"Everytime he brings a new toy into our room he meows...");
+    
+// using a for statement, output how many times he meows!
     for(int x = 0; x < 5; x++){
         NSLog(@"Meow!");
     }
     
-    
-    
-    
-    
-    
-/*
-    
-// declaring my variables
-    float number = 10.01f;
-    float yetAnotherNumber = 78.90f;
-    int anotherNumber = 23;
-    
-// declaring boolean type
-    BOOL thisIsEitherYesOrNo = YES;
-    
-// this outputs the value of number with 2 decimal places(%.2f)
-    NSLog(@"Here is the value of number represented as a float : %.2f", number);
-    
-// casting the number float into a integer value (int)number
-    NSLog(@"Here is the value of number represented as an integer : %d", (int)number);
-    
-    
-// using if, else if, and else statements as well as boolean logic
-    
-    if ((anotherNumber != 23) && (yetAnotherNumber == 78.90f)){
-        NSLog(@"The two number values are correct");
-    }
-    else if((anotherNumber > 23) || (yetAnotherNumber < 78.90f)){
-        NSLog(@"Both numbers are higher than they need to be");
-    }
-    else if((anotherNumber == 23) && (thisIsEitherYesOrNo == YES)){
-        NSLog(@"The anotherNumber variable is 23 and thisIsEitherYesOrNo is YES");
+// using a while loop, I tell him to shut up!!
+    NSLog(@"Everytime he comes into the room, I tell him ");
+    int o = 0;
+    while (o < 5){
+        NSLog(@"Shut up!!");
+        o++;
     }
     
-    
-// using a for loop to loop through an NSLog output 10 times.
-    for(int i = 0; i < 10; i++){
-        NSLog(@"hellooooooooo.....");
-    }
-    
-// using a while loop
-    int x = 0;
-    while (x < 10) {
-        NSLog(@"This is inside a while loop");
-        // integer x is incremented 1 time each time the loop goes through.
-        x++;
-    }
-    
-// nested loop
+// Using a nested loop
     for(int a = 0; a < 5; a++){
-        NSLog(@"variable a = %d", a);
-        
-        // inner loop
-        for(int l = 0; l < 5; l++){
-            NSLog(@"variable x = %d", l);
+        NSLog(@"%d", a);
+        for(int b = 0; b < 5; b++){
+            NSLog(@"%d", b);
         }
     }
- */
+    
+    
+    
+
 //<--------------------------------------------- end -------------------------------------------------->
     
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
