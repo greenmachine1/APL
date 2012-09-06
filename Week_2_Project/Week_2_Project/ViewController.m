1f1738ea451d2815c1ac49f257b3de72ab3f3af7
Cory <greenmachine@fullsail.edu>
2012-09-06 02:11:36 -0700

    modified my main files

--- Week_2_Project/Week_2_Project/ViewController.m
+++ Week_2_Project/Week_2_Project/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad
 {
+    
+    self.view.backgroundColor = [UIColor greenColor];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
