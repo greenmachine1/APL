aed2b2d3c3b1b0fe37008fed67401ecf6a96d78d
Cory <greenmachine@fullsail.edu>
2012-09-11 20:03:48 -0700

    working on my nsnumber function

--- Week_3_project/Week_3_project/ViewController.m
+++ Week_3_project/Week_3_project/ViewController.m
@@ -16,7 +16,19 @@
 
 - (void)viewDidLoad
 {
+    /*
+     Create a function called Add. This function will take two NSInteger or int types and return the result of an addition between these two.
+     Create a BOOL function called Compare that takes two NSInteger values. Return YES or NO based on whether the values are equal.
+     Create a function called Append. This function will take two NSStrings and return a new NSString containing the appended strings using an NSMutableString and the Append method.
+     Call the Append function with two NSStrings. Capture the result and display a UIAlertView with the appended string using displayAlertWithString.
+     Create a function called DisplayAlertWithString. This function will take as a parameter an NSString.
+     Call the Add function passing in two integer values. Capture the return of this function into a variable.
+     Bundle the returned integer into an NSNumber and then convert it to a NSString and pass it to the DisplayAlertWithString function.
+     Give it some text for the title. The message will read, "The number is 00". Replace the 00 with the integer passed into the function.
+     Call the Compare function with two integer values. If Compare returns YES, display an UIAlertView both with the input values and the result using the DisplayAlertWithString function
+     
     
+    */
     NSString *firstStringValue = [[NSString alloc] initWithString:@"Cory"];
     NSString *secondStringValue = [[NSString alloc] initWithString:@"Green"];
     
@@ -40,7 +52,9 @@
     int newNumber = [self add:firstIntegerValue second:secondIntegerValue];
     
     // assigning newNSNumber with the result of newNumber
-    NSNumber *newNSNumber = [[NSNumber alloc] initWithInt:newNumber];
+    NSNumber *newNSNumber = [[NSNumber alloc] initWithInteger:newNumber];
+    
+    NSLog(@"%@", newNSNumber);
     
     
     
